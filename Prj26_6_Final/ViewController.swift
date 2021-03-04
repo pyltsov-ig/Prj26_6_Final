5f7fb64650a2f1ce99529f7d806eeac39649a6dd
IGP <pyltsov@gmail.com>
2021-03-04 18:17:50 +0100

    Initial Commit

---
+++ Prj26_6_Final/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prj26_6_Final
+//
+//  Created by ИГОРЬ on 04/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
