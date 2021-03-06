212920ae6ecf455abbf0e068b0d0cb730993f663
IGP <pyltsov@gmail.com>
2021-03-06 21:43:20 +0100

    Итог 1-го задания

--- Prj26_6_Final/SceneDelegate.swift
+++ Prj26_6_Final/SceneDelegate.swift
@@ -14,11 +14,14 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
+        
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         self.window?.windowScene = windowScene
         window?.rootViewController = ViewController()
         window?.makeKeyAndVisible()
+        
+        
     }
 }
 
--- Prj26_6_Final/ViewController.swift
+++ Prj26_6_Final/ViewController.swift
@@ -9,42 +9,106 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let defaultValue = UserDefaults.standard
+    let key:String = "ISDARK"
+    
+    // флаг для вида экрана. true - светлая тема, false - темная тема
+    var isDark:Bool {
+        set {
+            defaultValue.setValue(newValue, forKey: key)
+        }
+        get {
+            return defaultValue.bool(forKey: key)
+        }
+    }
+    
+    // label. ни зачем, просто так, чтобы было.
     private(set) var myLabel: UILabel = {
         let myLabel = UILabel()
-       // myLabel.center.x =
-       // myLabel.center.y = 300
-        myLabel.frame = CGRect(x: 100, y:500, width: 100, height: 60)
-        myLabel.textColor = .white
-        myLabel.text = "Я СМОГ !!!"
+        myLabel.frame = CGRect(x: 0, y:100, width: 414, height: 60)
+        myLabel.textColor = .black
+        myLabel.textAlignment = .center
+        //myLabel.font.
+        myLabel.text = "26.6. ИТОГОВЫЙ ПРОЕКТ (Часть 1)"
         return myLabel
     }()
     
-    //@IBOutlet weak var newLabel: UILabel!
+    //кнопку можно использовать, как альтернативный свичу переключатель
+    private(set) var myButton: UIButton = {
+        let myButton = UIButton(frame: CGRect(x: 157, y: 660, width: 100, height: 50))
+        myButton.backgroundColor = .red
+        myButton.layer.cornerRadius = myButton.frame.size.height / 2
+        myButton.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        myButton.setTitle("Нажми", for: .normal)
+        return myButton
+    }()
+    
+    // свич
+    private(set) var mySwitch: UISwitch = {
+        let mySwitch = UISwitch()
+        mySwitch.frame = CGRect(x: 182, y: 600, width: 49, height: 31)
+        mySwitch.addTarget(self, action: #selector(switchAction), for: .valueChanged)
+        return mySwitch
+    }()
+    
+    // картинка
+    private(set) var imageOne: UIImageView = {
+        let imageOne = UIImageView()
+        return imageOne
+    }()
+    
+    
+    
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .darkGray
         
-        
-        view.addSubview(myLabel)
-        
-        let button = UIButton(frame: CGRect(x:200,y:200,width: 100, height: 50))
-        button.backgroundColor = .red
-        //button.setTitle("Кнопошка", forState: .normal)
-        button.setTitle("Кнопка", for: .normal)
-        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
-        
-        self.view.addSubview(button)
-        
-//        newLabel.frame = CGRect(x: 200, y: 300, width: 100, height: 50)
-//        newLabel.text = "Hello"
-//        newLabel.backgroundColor = .blue
-//        self.view.addSubview(newLabel)
-        
+
+        self.view.addSubview(myLabel)
+        self.view.addSubview(myButton)
+        self.view.addSubview(mySwitch)
+        self.view.addSubview(imageOne)
+       
+        mySwitch.isOn = isDark
+        if isDark {
+            setDarkScreen()
+        } else {
+            setLightScreen()
+        }
     }
     
     @objc func buttonAction() {
-        self.myLabel.text = "НАЖАЛ"
+        if mySwitch.isOn {
+            mySwitch.isOn = false
+        } else {
+            mySwitch.isOn = true
+        }
+        switchAction()
+    }
+    
+    @objc func switchAction() {
+        isDark = mySwitch.isOn
+        if isDark {
+            setDarkScreen()
+        } else {
+            setLightScreen()
+        }
+    }
+    
+    func setLightScreen() {
+        imageOne.frame = CGRect(x:84, y:270, width: 250, height: 250)
+        imageOne.image = UIImage(named: "lemasque")
+        view.backgroundColor = .white
+        myButton.backgroundColor = .blue
+        myLabel.textColor = .black
+    }
+    
+    func setDarkScreen() {
+        imageOne.frame = CGRect(x:23, y:270, width: 372, height: 250)
+        imageOne.image = UIImage(named: "dreaminп")
+        view.backgroundColor = .darkGray
+        myButton.backgroundColor = .red
+        myLabel.textColor = .white
     }
 
 
